13c0ccf763e3c788844327c2b5810b2a132a860b
VivBelwal <04vivek10belwal.com>
2022-09-28 15:06:16 +0530

    added come files

---
+++ nature-basket/src/Components/Navbar.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {Box,Container, Flex,Image, Input,Button} from "@chakra-ui/react";
+
+function Navbar (){
+
+    return <Container>
+        <Flex>
+
+
+        <Box>
+
+         <Image src="" alt="Nature basket" />
+
+        </Box>
+
+
+
+
+
+        <Box>
+
+            <Flex>
+
+                <Box>Online Slots Availability</Box>
+                <Box>Fresh & Fast</Box>
+                <Box></Box>
+                <Box></Box>
+                <Box></Box>
+                <Box></Box>
+            </Flex>
+
+
+
+            <Flex>
+                <Input></Input>
+                <Button></Button>
+                <Box></Box>
+                <Box></Box>
+            </Flex>
+        </Box>
+
+        </Flex>
+        <Box>
+            
+        </Box>
+    </Container>
+}
+
+export default Navbar 
